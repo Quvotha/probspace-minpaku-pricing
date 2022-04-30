c21c9444b4b3c5f50ac1074ea58a33bfc0d05647
Quvotha <nndropout1+github@gmail.com>
2022-05-01 01:34:24 +0900

    [add] create `LocationClustering`

--- scripts/feature_engineering.py
+++ scripts/feature_engineering.py
@@ -1,3 +1,4 @@
+from math import isclose, radians, atan, tan, sin, cos, sqrt, atan2, degrees, pi
 from typing import Dict, List, Tuple
 
 from geopy.distance import geodesic
@@ -5,6 +6,9 @@ from joblib import delayed, Parallel
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+
 
 _BASE_DATE = '2015-05-01'
 
@@ -246,3 +250,160 @@ class NeighborMinpakuCounter:
         mask = self.distances.loc[row_id] <= km
         count = mask.sum() - 1  # -1 は自分自身を除外するため
         return count
+
+
+def vincenty_inverse(lat1, lon1, lat2, lon2, ellipsoid=None, default=np.nan):
+    """2地点の経度緯度情報から距離と方位角を計算する。
+
+    ほぼ以下のサイトのコードをコピペした。
+    https://qiita.com/r-fuji/items/5eefb451cf7113f1e51b
+    """
+
+    # 楕円体
+    ELLIPSOID_GRS80 = 1  # GRS80
+    ELLIPSOID_WGS84 = 2  # WGS84
+
+    # 楕円体ごとの長軸半径と扁平率
+    GEODETIC_DATUM = {
+        ELLIPSOID_GRS80: [
+            6378137.0,         # [GRS80]長軸半径
+            1 / 298.257222101,  # [GRS80]扁平率
+        ],
+        ELLIPSOID_WGS84: [
+            6378137.0,         # [WGS84]長軸半径
+            1 / 298.257223563,  # [WGS84]扁平率
+        ],
+    }
+
+    # 反復計算の上限回数
+    ITERATION_LIMIT = 1000
+
+    # 差異が無ければ0.0を返す
+    if isclose(lat1, lat2) and isclose(lon1, lon2):
+        return 0.0
+
+    # 計算時に必要な長軸半径(a)と扁平率(ƒ)を定数から取得し、短軸半径(b)を算出する
+    # 楕円体が未指定の場合はGRS80の値を用いる
+    # a, ƒ = GEODETIC_DATUM.get(ellipsoid, GEODETIC_DATUM.get(ELLIPSOID_GRS80))
+    a, ƒ = 6378137.0, 1 / 298.257222101
+    b = (1 - ƒ) * a
+
+    φ1 = radians(lat1)
+    φ2 = radians(lat2)
+    λ1 = radians(lon1)
+    λ2 = radians(lon2)
+
+    # 更成緯度(補助球上の緯度)
+    U1 = atan((1 - ƒ) * tan(φ1))
+    U2 = atan((1 - ƒ) * tan(φ2))
+
+    sinU1 = sin(U1)
+    sinU2 = sin(U2)
+    cosU1 = cos(U1)
+    cosU2 = cos(U2)
+
+    # 2点間の経度差
+    L = λ2 - λ1
+
+    # λをLで初期化
+    λ = L
+
+    # 以下の計算をλが収束するまで反復する
+    # 地点によっては収束しないことがあり得るため、反復回数に上限を設ける
+    for i in range(ITERATION_LIMIT):
+        sinλ = sin(λ)
+        cosλ = cos(λ)
+        sinσ = sqrt((cosU2 * sinλ) ** 2 + (cosU1 * sinU2 - sinU1 * cosU2 * cosλ) ** 2)
+        cosσ = sinU1 * sinU2 + cosU1 * cosU2 * cosλ
+        σ = atan2(sinσ, cosσ)
+        sinα = cosU1 * cosU2 * sinλ / sinσ
+        cos2α = 1 - sinα ** 2
+        cos2σm = cosσ - 2 * sinU1 * sinU2 / cos2α
+        C = ƒ / 16 * cos2α * (4 + ƒ * (4 - 3 * cos2α))
+        λʹ = λ
+        λ = L + (1 - C) * ƒ * sinα * (σ + C * sinσ * (cos2σm + C * cosσ * (-1 + 2 * cos2σm ** 2)))
+
+        # 偏差が.000000000001以下ならbreak
+        if abs(λ - λʹ) <= 1e-12:
+            break
+    else:
+        # 計算が収束しなかった場合
+        return default
+
+    # λが所望の精度まで収束したら以下の計算を行う
+    u2 = cos2α * (a ** 2 - b ** 2) / (b ** 2)
+    A = 1 + u2 / 16384 * (4096 + u2 * (-768 + u2 * (320 - 175 * u2)))
+    B = u2 / 1024 * (256 + u2 * (-128 + u2 * (74 - 47 * u2)))
+    Δσ = B * sinσ * (cos2σm + B / 4 * (cosσ * (-1 + 2 * cos2σm **
+                     2) - B / 6 * cos2σm * (-3 + 4 * sinσ ** 2) * (-3 + 4 * cos2σm ** 2)))
+
+    # 2点間の楕円体上の距離
+    s = b * A * (σ - Δσ)
+
+    # 各点における方位角
+    α1 = atan2(cosU2 * sinλ, cosU1 * sinU2 - sinU1 * cosU2 * cosλ)
+    α2 = atan2(cosU1 * sinλ, -sinU1 * cosU2 + cosU1 * sinU2 * cosλ) + pi
+
+    if α1 < 0:
+        α1 = α1 + pi * 2
+    return degrees(α1)
+
+
+class LocationClustering(BaseEstimator, TransformerMixin):
+
+    def __init__(self, kmeans_args: dict = {'n_clusters': 9, 'random_state': 901}):
+        self.kmeans_args = kmeans_args
+
+    def fit(self, X: pd.DataFrame, y=None) -> object:
+        """位置情報のクラスタリングを行う。
+
+        Parameters
+        ----------
+        X : pd.DataFrame
+            経度緯度を `latitude`, `longitude` のカラムに設定しておくこと。 
+        y : 
+            無視する。
+
+        Returns
+        -------
+        self : object
+            Fit した結果。
+        """
+        scaler = StandardScaler().fit(X[['latitude', 'longitude']])
+        X_scaled = scaler.transform(X[['latitude', 'longitude']])
+        kmeans = KMeans(**self.kmeans_args).fit(X_scaled)
+        self.scaler_ = scaler
+        self.kmeans_ = kmeans
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """Kmeans によるクラスタリング結果を用いた特徴量を返す。
+
+        Parameters
+        ----------
+        X : pd.DataFrame
+            民泊の経度緯度を `latitude`, `longitude` のカラムに設定しておくこと。
+
+        Returns
+        -------
+        features : pd.DataFrame
+            特徴量。次の2つのカラムを持つ。
+            - `cluster_id`: 民泊がどのクラスターに属しているのかを示す
+            - `km_from_center`: 民泊が自身が所属するクラスターのセントロイドから何 km 離れているかを示す
+            - `azimuth`: クラスターの中心から民泊までの方位角
+        """
+        assert not X.index.duplicated().any()
+        features = []
+        # 所属するクラスターを得る
+        X_scaled = self.scaler_.transform(X[['latitude', 'longitude']].copy())
+        cluster_ids = self.kmeans_.predict(X_scaled)
+        # クラスターの中心からの距離を得る
+        centroids = self.scaler_.inverse_transform(self.kmeans_.cluster_centers_, copy=True)
+        for cluster_id, latitude, longitude in zip(cluster_ids, X['latitude'].to_numpy(), X['longitude'].to_numpy()):
+            centroid = centroids[cluster_id]
+            latitude_center, longitude_center = centroid
+            distance = geodesic((latitude, longitude), (latitude_center, longitude_center)).km
+            azimuth = vincenty_inverse(latitude_center, longitude_center, latitude, longitude, 2)
+            features.append([cluster_id, distance, azimuth])
+        features = pd.DataFrame(data=features, index=X.index, columns=['cluster_id', 'km_from_center', 'azimuth'])
+        return features
